0ccd8d370819846be5ef690529a1e8a428776619
Anderson Piotto <piottok10@gmail.com>
2022-11-11 22:39:54 -0300

    metodo default

--- src/br/com/ada/petshop/modelo/Internavel.java
+++ src/br/com/ada/petshop/modelo/Internavel.java
@@ -3,4 +3,13 @@ package br.com.ada.petshop.modelo;
 public interface Internavel {
 
     boolean estaDoente();
+
+    /* a partir do java 8, surgiu esse metodo default, que possui corpo
+      e portanto não precisa ser implemtado nas classes que implementam a interface
+      isso é util para quando você deseja inserir um comportamento na interface e não que obrigar as classes a
+      fazerem  implementação ;)
+     */
+    default void metodo(){
+        System.out.println("Chamando o metodo default");
+    }
 }
