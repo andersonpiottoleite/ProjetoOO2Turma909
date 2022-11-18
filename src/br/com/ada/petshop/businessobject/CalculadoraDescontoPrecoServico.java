ff8f05c525c77d7642ab2ea9d1523a84d68d2b78
Anderson Piotto <piottok10@gmail.com>
2022-11-18 13:05:01 -0300

    preparacao para aplicacao do solid

---
+++ src/br/com/ada/petshop/businessobject/CalculadoraDescontoPrecoServico.java
@@ -0,0 +1,29 @@
+package br.com.ada.petshop.businessobject;
+
+import br.com.ada.petshop.enumeracao.TipoPagamentoEnum;
+import br.com.ada.petshop.modelo.Pagamento;
+import br.com.ada.petshop.modelo.Servico;
+
+import java.math.BigDecimal;
+
+public class CalculadoraDescontoPrecoServico {
+
+    // sempre que uma nova foram de pagamento for criada, teremos que adiconar um novo isso
+    // nossa clase estará aberta para alterações
+    // podendo gerar bugs ao sofrer alterações
+    // Podemos aplicar o O do SOLID?
+    // O - OCP - Open-Closed Principle - Principio aberto e fechado?
+    public void aplicaDesconto(Servico servico, Pagamento pagamento){
+
+        if (TipoPagamentoEnum.CREDITO.equals(pagamento.getTipoPagamento())){
+            //se for credito, desconto de R$ 1.00
+            BigDecimal valorComDesconto = servico.getValor().subtract(new BigDecimal(1.0));
+            servico.setValor(valorComDesconto);
+
+        } else if (TipoPagamentoEnum.DEBITO.equals(pagamento.getTipoPagamento())){
+            //se for debito, desconto de R$ 10.00
+            BigDecimal valorComDesconto = servico.getValor().subtract(new BigDecimal(10.0));
+            servico.setValor(valorComDesconto);
+        }
+    }
+}
